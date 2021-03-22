c912f585af0e352a1f47d02fd7a6ae8b9231e6af
jsuk10 <jsuk10@naver.com>
2021-03-22 21:42:20 +0900

    feat : Add Button Info

---
+++ Assets/Script/UI/ButtonInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInfo : MonoBehaviour
+{
+    [SerializeReference]
+    private ButtonType currentType;
+    [SerializeField]
+    private bool isStartActive = true;
+
+    public ButtonType CurrentType
+    {
+        get { return currentType; }
+    }
+    private void Awake()
+    {
+        this.gameObject.SetActive(isStartActive);
+    }
+}
---
+++ Assets/Script/UI/ButtonTypes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ButtonType
+{
+    StartGame,
+    Load,
+    Credits,
+    OptionOn,
+    OptionOff,
+    SettingOn,
+    SettingOFF,
+    Sound,
+    Quit
+};
+
+public class ButtonTypes : MonoBehaviour
+{
+}
