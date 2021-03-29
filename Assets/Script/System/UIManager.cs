f0cdb8668d3b0aa6e6445efc3162c76cb1c68a70
jsuk10 <jsuk10@naver.com>
2021-03-29 21:39:28 +0900

    refactor  : UIManager More Easier
    UIManager가 너무 복잡하게 만들어져 있어서 다시 쉽게 바꾸었다.

--- Assets/Script/System/UIManager.cs
+++ Assets/Script/System/UIManager.cs
@@ -1,44 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class UIManager : MonoBehaviour
+[System.Serializable]
+public class UIMenu
 {
-    [SerializeField]
-    ButtonInfo[] UImenu;
-    Dictionary<ButtonType, GameObject> uiDictionary = new Dictionary<ButtonType, GameObject>();
+    public GameObject loadOption;
+    public GameObject settingOption;
+    public GameObject creadit;
+    public Text[] playerNameText = new Text[3];
+}
 
-    private void Awake()
+public class UIManager : Singleton<UIManager>
+{
+    public UIMenu uiMenu;
+    private PlayerName playerName;
+
+    //초기화 (awake)
+    public override void init()
     {
-        init();
+        //딕셔너리에 버튼마다 설정한 타입을 넣어둠.
     }
-    public void init()
+
+    private void Start()
     {
-        for (int i = 0; i < UImenu.Length; i++)
+        //플레이어 네임을 가지고옴.
+        playerName = PlayerNameController.Instance.PlayerName;
+        SetName();
+        OffMenu();
+    }
+
+    private void OffMenu()
+    {
+        uiMenu.loadOption.SetActive(false);
+        uiMenu.settingOption.SetActive(false);
+        uiMenu.creadit.SetActive(false);
+
+    }
+    private void SetName()
+    {
+        for (int i = 0; i < playerName.playerNames.Length; i++)
         {
-            uiDictionary.Add(UImenu[i].CurrentType, UImenu[i].gameObject);
+            if (playerName.playerNames[i] == null)
+            {
+                uiMenu.playerNameText[i].text = "새 데이터 만들기";
+            }
+            else
+            {
+                uiMenu.playerNameText[i].text = playerName.playerNames[i];
+            }
         }
     }
-    public void GameStart()
+
+
+
+    public void LoadOnOff()
     {
-        SceneManager.LoadScene("Start");
-    }
-    public void Load()
-    {
-        Debug.Log("Load");
-    }
-    public void SettingOnOff()
-    {
-        uiDictionary[ButtonType.SettingOn].SetActive(!uiDictionary[ButtonType.SettingOn].activeSelf);
-        uiDictionary[ButtonType.SettingOFF].SetActive(!uiDictionary[ButtonType.SettingOn].activeSelf);
-    }
-    public void OptionOnOff()
-    {
-        uiDictionary[ButtonType.OptionOn].SetActive(!uiDictionary[ButtonType.OptionOn].activeSelf);
-        uiDictionary[ButtonType.OptionOff].SetActive(!uiDictionary[ButtonType.OptionOn].activeSelf);
+        uiMenu.loadOption.SetActive(!uiMenu.loadOption.activeSelf);
     }
 
+    //세팅을 껏다 킬때 사용함
+    public void SettingOnOff()
+    {
+        //토글형식으로 생각하면 될듯
+        uiMenu.settingOption.SetActive(!uiMenu.settingOption.activeSelf);
+    }
+    //옵션을 껐다 킬때 사용함
+    public void CreditOnOff()
+    {
+        //마찬가지로 토글
+        uiMenu.creadit.SetActive(!uiMenu.creadit.activeSelf);
+    }
+
+    //게임 나갈 경우
+    //플렛폼별로 써놓음
     public void ExitGame()
     {
 #if UNITY_EDITOR
