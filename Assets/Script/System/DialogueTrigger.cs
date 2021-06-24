cbc15455fbd81f19a478f57dddbb9a95bb71458f
jsuk10 <jsuk970407@gmail.com>
2021-06-25 04:06:27 +0900

    test : Add Test

---
+++ Assets/Script/System/DialogueTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public void Trigger()
+    {
+        Debug.Log("trigger");
+        DialogueManager.Instance.Begin(1,5);
+    }
+}
