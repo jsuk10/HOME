e3fe878910dc1e7e2061577d36716e419509ec4c
jsuk10 <jsuk970407@gmail.com>
2021-06-25 04:05:41 +0900

    chore : Add remark

--- Assets/Script/Manager/SceneManager.cs
+++ Assets/Script/Manager/SceneManager.cs
@@ -11,7 +11,10 @@ public class SceneManager : Singleton<SceneManager>
         DontDestroyOnLoad(gameObject);
     }
 
-
+    /// <summary>
+    /// 신 옮겨주는 함
+    /// </summary>
+    /// <param name="sceneName">신의 이름을 넣는곳, 잘못할 가능성 생각해서 enum으로 관리함</param>
     public void LoadScene(SceneName sceneName)
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName.ToString());
