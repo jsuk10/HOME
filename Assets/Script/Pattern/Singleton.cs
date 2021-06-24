af5c8ee771ce05f92c7d6d7fd2e1804739197d4f
jsuk10 <jsuk970407@gmail.com>
2021-06-25 04:03:59 +0900

    feat : Add singleton not using unity

--- Assets/Script/Pattern/Singleton.cs
+++ Assets/Script/Pattern/Singleton.cs
@@ -7,6 +7,27 @@ public interface ISingleton
     void Init();
 }
 
+public abstract class NonUnitySingleton<T> : ISingleton where T : NonUnitySingleton<T>, new()
+{
+    public abstract void Init();
+
+    private static T _instance;
+
+    public static T Instance => _instance != null ? _instance : GetInstanceObject();
+
+    protected static T GetInstanceObject()
+    {
+        if (_instance != null) return _instance;
+
+        _instance = new T();
+
+        var _singleton = _instance as NonUnitySingleton<T>;
+
+        _singleton.Init();
+        return _instance;
+    }
+}
+
 public abstract class Singleton<T> : MonoBehaviour, ISingleton where T : Singleton<T>
 {
     /// <summary>
@@ -14,10 +35,11 @@ public abstract class Singleton<T> : MonoBehaviour, ISingleton where T : Singlet
     /// awake에서 실행하게 했음
     /// </summary>
     public abstract void Init();
+    public static bool IsNull => _instance == null;
 
-    protected static T _instance;
+    private static T _instance;
 
-    public static T Instance => _instance != null ? _instance : GetInstanceObject();
+    public static T Instance => !IsNull ? _instance : GetInstanceObject();
 
     /// <summary>
     /// 싱글톤을 만들어주고 instance 반환하는 함수
@@ -25,12 +47,9 @@ public abstract class Singleton<T> : MonoBehaviour, ISingleton where T : Singlet
     /// </summary>
     protected static T GetInstanceObject()
     {
-        if (_instance != null) return _instance;
-
+        if (!IsNull) return _instance;
         _instance = FindObjectOfType<T>();
-
         var _singleton = _instance as Singleton<T>;
-
         _singleton.Init();
 
         return _instance;
