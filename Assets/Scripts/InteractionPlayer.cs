4237c6efc310ee8a7217177b079376c726f70c2b
Zeya <50080904+ZeyaGun@users.noreply.github.com>
2021-08-04 19:29:06 +0900

    feat: InteractionPlayer.cs
    현재 상호작용 가능한 오브젝트를 큐에 저장해서
    
    상호작용시 사용하는 클래스

---
+++ Assets/Scripts/InteractionPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionPlayer : MonoBehaviour
+{
+    Queue<ObjectInteractable> queueObject;
+
+    void Awake()
+    {
+        queueObject = new Queue<ObjectInteractable>();
+    } 
+
+    void Update()
+    {
+        InputProcess();
+    }
+
+    /// <summary>
+    /// 입력 처리, 상호작용 키를 누르면 큐에서 오브젝트를 반환하여 상호작용 함수 호출
+    /// </summary>
+    void InputProcess()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            var dequeuedObject = queueObject.Dequeue();
+            if(dequeuedObject != null)
+            {
+                dequeuedObject.IsEnqueued = false;
+                dequeuedObject.Interaction();
+            }
+        
+        }
+    }
+
+    /// <summary>
+    /// 외부에서 Enqueue할때 쓰는 함수. 충돌 상태일때 오브젝트가 스스로를 삽입 요청
+    /// </summary>
+    /// <param name="objectInteractable"></param>
+    public void EnqueueObject(ObjectInteractable objectInteractable)
+    {
+        queueObject.Enqueue(objectInteractable);
+    }
+
+    /// <summary>
+    /// 외부에서 Dequeue할때 쓰는 함수. 일정 범위를 벗어낫을때 오브젝트가 스스로를 반환 요청
+    /// </summary>
+    /// <returns></returns>
+    public ObjectInteractable DequeueObject()
+    {
+        return queueObject.Dequeue();
+    }
+}
