dbe1dc835607524ba41a11c9bff3678ad1e22050
Zeya <50080904+ZeyaGun@users.noreply.github.com>
2021-08-04 13:54:02 +0900

    상호작용 관련 예시 스크립트 구현

---
+++ Assets/Scripts/Object/InteractionImpl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IInterAction
+{
+    void interaction();
+}
+public class InteractionDropPicture : MonoBehaviour, IInterAction
+{
+    [SerializeField]
+    GameObject Player;
+    
+    float interactionRange = 1f;
+
+    private void Update() 
+    {
+        if(Vector3.Distance(this.transform.position, Player.transform.position) <= interactionRange)
+        {
+            if(Input.GetAxis("Fire1") == 1)
+                interaction();
+        }
+    }
+
+    public void interaction()
+    {
+
+        
+    }
+}
