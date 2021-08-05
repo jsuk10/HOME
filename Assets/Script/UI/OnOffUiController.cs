79a46553e15ba106222fb47b56358bceb96140fc
jsuk10 <jsuk970407@gmail.com>
2021-08-05 21:39:19 +0900

    feat : OnOffUIController
    키고 끄는 UI 를 만들기 위해 만듬
    하위 오브젝트로는 아래와 같다
    현재는 끄는 기능만 구현하고 세부 기능은 차차 구현할 예정
    
    1.Credit
     게임제작자를 넣은 창
    2.Setting
     세팅을 해주는 창
     아직 구현이 안되었으며 소리 조절 및 옵션 삽입 예정
    3.Load
     게임데이터를 로드 하는 창

---
+++ Assets/Script/UI/OnOffUiController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnOffUiController : AddUIButtonEvent
+{
+    #region field
+    private GameObject[] gamedatas;
+    #endregion
+    #region InheritanceFunction
+    public override void Init()
+    {
+        Set();
+    }
+    #endregion
+
+    #region Function
+    /// <summary>
+    /// 이벤트 할당을 위해 Init 에서 실행하는 함수
+    /// </summary>
+    public override void Set()
+    {
+        AddButtonEvent("Credit/CreditOffButton", () => SetTargetView(LobbyManager.Instance.ObjectDirctory["Credit"], false));
+        AddButtonEvent("Setting/SettingOffButton", () => SetTargetView(LobbyManager.Instance.ObjectDirctory["Setting"], false));
+        AddButtonEvent("GameStart/LoadOffButton", () => SetTargetView(LobbyManager.Instance.ObjectDirctory["Load"], false));
+    }
+
+
+    #endregion
+
+    #region Event
+
+
+    #endregion
+}
