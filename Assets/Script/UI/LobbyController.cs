564a5d8017f0945c8e0a3eed4df92a7ed0c5ccad
jsuk10 <jsuk970407@gmail.com>
2021-08-05 21:48:53 +0900

    fix : Option on/off does not work properly
    옵선 온오프가 상위 객체를 꺼서 정상 작동 안되는 버그를 수정함.

--- Assets/Script/UI/LobbyController.cs
+++ Assets/Script/UI/LobbyController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LobbyController : AddUIEvent
+public class LobbyController : AddUIButtonEvent
 {
     #region field
     private GameObject[] gamedatas;
@@ -11,7 +11,7 @@ public class LobbyController : AddUIEvent
     #region InheritanceFunction
     public override void Init()
     {
-        SetName();
+        // SetName();
         Set();
     }
     #endregion
@@ -22,59 +22,38 @@ public class LobbyController : AddUIEvent
     /// </summary>
     public override void Set()
     {
-        AddButtonEvent("GameStartButton", () =>
-        {
-            SetView(false);
-            SetView(LobbyManager.Instance.ObjectDirctory["Load"], true);
-        });
-        AddButtonEvent("CreidtButton", () =>
-        {
-            SetView(false);
-            SetView(LobbyManager.Instance.ObjectDirctory["Credit"], true);
-        });
-        AddButtonEvent("SettingButton", () =>
-        {
-            SetView(false);
-            SetView(LobbyManager.Instance.ObjectDirctory["Setting"], true);
-        });
+        AddButtonEvent("GameStartButton", () => SetTargetView(LobbyManager.Instance.ObjectDirctory["Load"], true));
+        AddButtonEvent("CreidtButton", () => SetTargetView(LobbyManager.Instance.ObjectDirctory["Credit"], true));
+        AddButtonEvent("SettingButton", () => SetTargetView(LobbyManager.Instance.ObjectDirctory["Setting"], true));
         AddButtonEvent("ExitButton", () => ExitGame());
     }
 
     /// <summary>
     /// 데이터 로드 매뉴 불러오기
     /// </summary>
-    private void SetName()
-    {
-        var playerName = PlayerData.Instance.PlayerName.playerData;
-        for (int i = 0; i < playerName.Length; i++)
-        {
-            if (playerName[i] == null)
-            {
-                Debug.Log("데이터 존재");
-                // SetText(LobbyManager.Instance.ObjectDirctory[$"Data{i + 1}"], "새 데이터 만들기");
-                // AddButtonEvent(gamedatas[i], MakeNewData);
-            }
-            else
-            {
-                // SetText(LobbyManager.Instance.ObjectDirctory[$"Data{i + 1}"], playerName[i].name);
-                // AddButtonEvent(gamedatas[i], LoadData);
-            }
-        }
-    }
+    // private void SetName()
+    // {
+    //     var playerName = PlayerData.Instance.PlayerName.playerData;
+    //     for (int i = 0; i < playerName.Length; i++)
+    //     {
+    //         if (playerName[i] == null)
+    //         {
+    //             Debug.Log("데이터 존재");
+    //             // SetText(LobbyManager.Instance.ObjectDirctory[$"Data{i + 1}"], "새 데이터 만들기");
+    //             // AddButtonEvent(gamedatas[i], MakeNewData);
+    //         }
+    //         else
+    //         {
+    //             Debug.Log("데이터 없음 ");
+
+    //             // SetText(LobbyManager.Instance.ObjectDirctory[$"Data{i + 1}"], playerName[i].name);
+    //             // AddButtonEvent(gamedatas[i], LoadData);
+    //         }
+    //     }
+    // }
     #endregion
 
     #region Event
-    private void MakeNewData()
-    {
-        //데이터 생성
-        //LoadScene(SceneName.inGame);
-    }
-    private void LoadData()
-    {
-        //데이터 할당
-        //LoadScene(SceneName.inGame);
-    }
-
 
     #endregion
 }
