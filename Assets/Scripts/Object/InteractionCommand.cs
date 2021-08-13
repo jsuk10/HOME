a316a82913dc24c3dbd979eda9523d7ffc85b25f
Zeya <50080904+ZeyaGun@users.noreply.github.com>
2021-08-13 17:35:02 +0900

    chore : etc
    접근지정자 및 변수명 수정

--- Assets/Scripts/Object/InteractionCommand.cs
+++ Assets/Scripts/Object/InteractionCommand.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 상호작용 시 실행되는 함수의 원형이 되는 클래스
+/// 상호작용 내용은 해당 클래스를 상속한 서브클래스에서 구현된다
+/// </summary>
 public class InteractionCommand : MonoBehaviour
 {
     private ObjectInteractable owner;
-    private void Interaction()
+    protected void Interaction()
     {
         Debug.Log(gameObject.name + " Interaction");
     }
@@ -13,6 +17,6 @@ public class InteractionCommand : MonoBehaviour
     {
         owner = transform.GetComponent<ObjectInteractable>();
         Debug.Log("Allocate Interaction");
-        owner.Interaction = Interaction;    
+        owner.delInteraction = Interaction;    
     }
 }
