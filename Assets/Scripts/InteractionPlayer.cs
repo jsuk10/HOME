bc3886d856021fe1333d1db7aa0e9a07aaa8f0fb
Zeya <50080904+ZeyaGun@users.noreply.github.com>
2021-08-13 17:33:14 +0900

    refactor : Interaction
    동시적으로 리스트를 수정하는 행동들이 인덱스 참조 오류를 발생시켜
    동작을 Command 클래스로 분리시켜 Command Queue가 순차적으로 명령을 처리하게 수정함

--- Assets/Scripts/InteractionPlayer.cs
+++ Assets/Scripts/InteractionPlayer.cs
@@ -5,57 +5,140 @@ using UnityEngine;
 public class InteractionPlayer : Singleton<InteractionPlayer>
 {
     List<ObjectInteractable> listObject;
+    public Queue<Command> queueCommand;
 
     public override void Init()
     {
         listObject = new List<ObjectInteractable>();
+        queueCommand = new Queue<Command>();
     } 
 
-    
     void Start()
     {
-        StartCoroutine("InputProcess");
+        StartCoroutine(nameof(InputProcess));
+        StartCoroutine(nameof(ExecuteCommandQueue));
     }
 
     /// <summary>
-    /// 입력 처리, 상호작용 키를 누르면 큐에서 오브젝트를 반환하여 상호작용 함수 호출
+    /// 입력 처리, 상호작용 키를 누르면 상호작용 커맨드를 큐에 삽입하고 딜레이를 준다
     /// </summary>
     public IEnumerator InputProcess()
     {
         while(true)
         {
             yield return new WaitUntil(() => Input.GetKey(KeyCode.E));
-            var tempListObject = listObject;
-            Debug.Log($"{tempListObject.Count}");
-            if(tempListObject.Count > 0)
-            {
-                var popedObject = PopObject(tempListObject[0]);
-                popedObject.Interaction();
-                popedObject.IsEnqueued = false;
-            }
+            queueCommand.Enqueue(new Command(this, null, nameof(Command.Interaction)));
             yield return new WaitForSeconds(0.3f);
         }
     }
 
     /// <summary>
-    /// 외부에서 Enqueue할때 쓰는 함수. 충돌 상태일때 오브젝트가 스스로를 삽입 요청
+    /// 매 프레임마다 큐를 체크해 명령이 들어왔다면 명령을 실행한다
     /// </summary>
-    /// <param name="objectInteractable"></param>
-    public void AddObject(ObjectInteractable objectInteractable)
+    /// <returns></returns>
+    public IEnumerator ExecuteCommandQueue()
     {
-        listObject.Add(objectInteractable);
+        while(true)
+        {
+            //Debug.Log(queueCommand.Count);
+            yield return new WaitUntil(() => 0 < queueCommand.Count);
+            queueCommand.Dequeue().Execute();
+        }
     }
 
     /// <summary>
-    /// 외부에서 Dequeue할때 쓰는 함수. 일정 범위를 벗어낫을때 오브젝트가 스스로를 반환 요청
+    /// 리스트를 변경하는 동작에 대한 정보를 가지고 있는 클래스
+    /// 외부 클래스인 Outer, 오브젝트인 Other, 실행할 명령인 CommandName을 인자로 받는다
     /// </summary>
-    /// <returns></returns>
-    public ObjectInteractable PopObject(ObjectInteractable objectInteractable)
+    public class Command
     {
-        int tempObjectIndex = listObject.FindIndex(x => x == objectInteractable);
-        var tempObject = listObject[tempObjectIndex];
-        listObject.RemoveAt(tempObjectIndex);
+        ObjectInteractable other;
+        InteractionPlayer outer;
+        delegate void DelCommand();
+        DelCommand delCommand;
 
-        return tempObject;
+        /// <summary>
+        /// 생성자에서 실행할 명령을 문자열로 받아 SetCommand를 통해 동작 결정
+        /// </summary>
+        /// <param name="Outer"></param>
+        /// <param name="Other"></param>
+        /// <param name="CommandName"></param>
+        public Command(InteractionPlayer Outer, ObjectInteractable Other, string CommandName)
+        {
+            outer = Outer;
+            other = Other;
+            SetCommand(CommandName);
+        }
+        
+        /// <summary>
+        /// 입력된 문자열에 따른 동작을 결정한다
+        /// </summary>
+        /// <param name="commandName"></param>
+        void SetCommand(string commandName)
+        {
+            if(commandName == nameof(Interaction))
+                delCommand = Interaction;
+            else if(commandName == nameof(AddObject))
+                delCommand = AddObject;
+            else if(commandName == nameof(RemoveObject))
+                delCommand = RemoveObject;
+        }
+
+        /// <summary>
+        /// 상호작용 시 호출되는 함수, 리스트의 첫 번째 원소를 뽑아서 제거하고 상호작용을 실행
+        /// </summary>
+        public void Interaction()
+        {
+            if(0 < outer.listObject.Count)
+            {
+                var popedObject = outer.listObject[0];
+                outer.listObject.RemoveAt(0);
+                popedObject.Interaction();
+            }
+        }
+
+        /// <summary>
+        /// 리스트에 오브젝트를 추가하는 함수
+        /// </summary>
+        /// <param name="objectInteractable"></param>
+        public void AddObject()
+        {
+            outer.listObject.Add(other);
+        }
+
+        /// <summary>
+        /// 리스트에 오브젝트를 제거하는 함수
+        /// </summary>
+        public void RemoveObject()
+        {
+            outer.listObject.Remove(other);
+        }
+
+        /// <summary>
+        /// 위의 동작이 실제로 수행되는 함수
+        /// delCommand
+        /// </summary>
+        public void Execute()
+        {
+            delCommand();
+        }
+    }
+
+    /// <summary>
+    /// Queue에 AddObject 명령을 삽입
+    /// </summary>
+    /// <param name="other"></param>
+    public void AddObject(ObjectInteractable other)
+    {
+        queueCommand.Enqueue(new Command(this, other, nameof(Command.AddObject)));
+    }
+
+    /// <summary>
+    /// Queue에 RemoveObject 명령을 삽입
+    /// </summary>
+    /// <param name="other"></param>
+    public void RemoveObject(ObjectInteractable other)
+    {
+        queueCommand.Enqueue(new Command(this, other, nameof(Command.RemoveObject)));
     }
 }
