7dd25c2499fe0953b4216e8fabd6e2072b224570
jsuk10 <jsuk970407@gmail.com>
2021-08-20 20:18:02 +0900

    feat : AddUi

---
+++ Assets/Script/UI/AddGameUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddGameUI : MonoBehaviour
+{
+    public void Add()
+    {
+        foreach (GameObject child in transform)
+        {
+            GameUIManager.Instance.ObjectDictionary.Add(child.name, child);
+            Debug.Log(child);
+        }
+    }
+    
+}
