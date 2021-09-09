c7fb5118e7dacf6f7529a6f49f8484612835728c
jsuk10 <jsuk970407@gmail.com>
2021-09-10 03:54:01 +0900

    feat : PlayPlayerAni
    반환값으로 애니메이션 플레이 시간을 주도록 함

--- Assets/Script/Manager/AnimationManager.cs
+++ Assets/Script/Manager/AnimationManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
-using UnityEngine.Animations;
 
 /// <summary>
 /// Dog와 Player의 Animation을 다뤄주는 이벤트
@@ -11,6 +10,7 @@ public class AnimationManager : Singleton<AnimationManager>
 {
     [SerializeField] private Animator PlayerAnimator;
     [SerializeField] private Animator DogAnimator;
+
 
     public override void Init() { }
 
@@ -30,34 +30,43 @@ public class AnimationManager : Singleton<AnimationManager>
             PlayerAnimator.runtimeAnimatorController = Resources.Load("PlayerAnimationController") as RuntimeAnimatorController;
         if (DogAnimator.runtimeAnimatorController == null)
             DogAnimator.runtimeAnimatorController = Resources.Load("DogAnimationController") as RuntimeAnimatorController;
+
     }
 
     /// <summary>
     /// 사람 애니메이션 실행해주는 함수
     /// </summary>
-    /// <param name="animationName">사람 애니메이션 네임</param>
-    public void PlayPlayerAni(string animationName)
+    /// <param name="animationName">애니메이션 이름</param>
+    /// <returns>애니메이션의 길이 리턴</returns>
+    public float PlayPlayerAni(string animationName)
     {
         try
         {
-            PlayerAnimator.Play(animationName);
+            this.PlayerAnimator.Play(animationName);
+            Debug.Log(PlayerAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.length);
+            //foreach(var a in playerClips) {
+            //    Debug.Log($"{a.name} : {a.length}") ;
+            //}
         }
-        catch (Exception e) {
+        catch (Exception e)
+        {
             Debug.Log($"{animationName} 은 Player에 없는 애니메이션 입니다.");
             Debug.Log(e);
         }
+        return PlayerAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.length;
     }
     /// <summary>
     /// 현재 움직일 수 있는 상태(디폴트,무브 상태일 경우)
     /// </summary>
     /// <param name="stateName"></param>
     /// <returns></returns>
-    public bool PlayerCanMove()
+    public bool PlayerIsMove()
     {
-        return PlayerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Defult")
-            || PlayerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Move")
+        return PlayerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Move")
             || PlayerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Run");
     }
+
+
     /// <summary>
     /// 강아지 애니매이션 실행해주는 스크립트
     /// </summary>
