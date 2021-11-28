7fbfb6fc5abe6bc2e9dd518a974458dcc06b29b5
Zeya <50080904+ZeyaGun@users.noreply.github.com>
2021-11-28 23:20:02 +0900

    feat : EventRoad 0~4
    임시 이벤트 커밋

---
+++ Assets/Scripts/GameEvent/Road/EventRoad1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventRoad1 : GameEvent
+{
+    override protected void InitEvent()
+    {   
+        
+    }
+
+    override public bool Condition()
+    {
+        if(caller != null)
+        {
+            if(caller.gameObject.name == "Driver")
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    override public IEnumerator EventAction()
+    {
+        //DialogueManager.Instance.Begin(38,42);
+        owner.StartStageEvent(2);
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/GameEvent/Road/EventRoad4.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventRoad4 : GameEvent
+{
+    override protected void InitEvent()
+    {   
+        
+    }
+
+    override public bool Condition()
+    {
+        //다음스테이지 이동
+        return true;
+    }
+
+    override public IEnumerator EventAction()
+    {
+        yield return null;
+    }
+}
